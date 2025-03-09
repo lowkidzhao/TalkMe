d8a53c755e887219150e664512bf1a8fe37e95da
pushme <1141846557@qq.com>
2025-03-09 14:07:20 +0800

    修改配置文件

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,12 @@
 import eslintConfig from '@electron-toolkit/eslint-config'
-import eslintConfigPrettier from '@electron-toolkit/eslint-config-prettier'
-import eslintPluginVue from 'eslint-plugin-vue'
 
 export default [
   { ignores: ['**/node_modules', '**/dist', '**/out'] },
   eslintConfig,
-  ...eslintPluginVue.configs['flat/recommended'],
   {
-    files: ['**/*.{js,jsx,vue}'],
+    files: ['**/*.{js,jsx}'],
     rules: {
-      'vue/require-default-prop': 'off',
-      'vue/multi-word-component-names': 'off'
+      // 移除所有 vue 相关规则
     }
-  },
-  eslintConfigPrettier
+  }
 ]
